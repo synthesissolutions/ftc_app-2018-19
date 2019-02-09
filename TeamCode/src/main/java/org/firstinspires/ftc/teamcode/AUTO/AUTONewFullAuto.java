811fd42dba20431fe7860a3ce0f7b8803a84c0d5
davidsherrick <dsherrick@synthesissolutions.com>
2019-02-09 13:03:22 -0600

    new auto code tuning (with backup options, no mineral detection or marker delivery)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/AUTONewFullAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/AUTONewFullAuto.java
@@ -5,11 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class AUTONewFullAuto extends AUTOMecanumAbstractPracticeBot {
     public void runOpMode() throws InterruptedException {
         startAutoOp();
-        setHangTowerPosition(-11222);
-        sleep(5555);
-        driveStraight(500, 0.3);
-        sleep(1000);
-        drive0(1400, -0.8);
+        setHangTowerPosition(-6350);
+        sleep(3000);
+        driveStraight(420, 0.3);
+        sleep(500);
+        drive0(2000, -1.0);
         stopMotors();
        /* if(getGyroCurrentHeading() < 0) {
             turnDegrees(0.4, getGyroCurrentHeading());
@@ -18,35 +18,37 @@ public class AUTONewFullAuto extends AUTOMecanumAbstractPracticeBot {
             turnDegrees(-0.4, getGyroCurrentHeading());
         }*/
         sleep(500);
-        driveStraight(300, 0.4);
+        driveStraight(750, 1.0);
         sleep(500);
-        deployMineralArm();
-        sleep(2500);
-        double moveAmount = this.motorFrontLeft.getCurrentPosition();
-        driveStraightAndColor(4000, -0.25);
+
+        int moveAmount = this.motorFrontLeft.getCurrentPosition();
+        driveStraightAndColor(4700, -0.25);
         moveAmount = (this.motorFrontLeft.getCurrentPosition() - moveAmount);
+        sleep(2000);
+        driveStraight(4700 - moveAmount, -1.0);
+        turnDegrees(0.4, 105);
         sleep(500);
-        servoMineralRotate.setPosition(1.0);
-        sleep(666);
-        driveStraight(550, 0.5);
-        retractMineralArm();
-        driveStraight(3000, 0.5);
-        turnDegrees(0.4, 100);
-
+        drive0(350, -0.8);
         if (moveAmount < 1600) {
-            deployMineralArm();
-            turnDegrees(0.4, 111);
-            driveStraight(1000, 0.5);
+            driveStraight(1500, -1.0);
+            turnDegrees(-0.4, 110);
+            driveStraight(400, -1.0);
         } else if (moveAmount < 2800) {
-            driveStraight(2700, 0.5);
-            turnDegrees(0.4, 111);
-            driveStraight(1000, 0.5);
+            driveStraight(3200, -1.0);
+            turnDegrees(-0.4, 110);
+            driveStraight(400, -1.0);
         } else if (moveAmount < 3900) {
-            driveStraight(3600, 0.5);
-            turnDegrees(0.4, 111);
-            driveStraight(1000, 0.5);
+            driveStraight(4500, -1.0);
+            turnDegrees(-0.4, 110);
+            driveStraight(400, -1.0);
         } else {
-
+            driveStraight(1500, -1.0);
+            turnDegrees(-0.4, 110);
+            driveStraight(400, -1.0);
         }
+
+//        driveStraight(4444, -0.5);
+//        turnDegrees(0.4, 107);
+//        driveStraight(1000, -0.5);
     }
 }
