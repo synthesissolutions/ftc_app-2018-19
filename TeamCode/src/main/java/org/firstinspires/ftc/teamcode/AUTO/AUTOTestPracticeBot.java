9125255137665f348b8b6752c423373e5dd3a16b
David Sherrick <dsherrick@synthesissolutions.com>
2019-02-09 10:42:31 -0600

    adding logging to test mineral detection code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/AUTOTestPracticeBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/AUTOTestPracticeBot.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.PreviousYear.AUTOMecanumAbstractCayenne;
 
@@ -45,6 +46,14 @@ public class AUTOTestPracticeBot extends AUTOMecanumAbstractPracticeBot {
         int redAtHighestBlue =0;
         int greenAtHighestBlue =0;
 
+        int reading = 0;
+        int encoder = 0;
+        ElapsedTime eTime = new ElapsedTime();
+        double startTime = 0.0;
+        double endTime = 0.0;
+
+        boolean twoSensors = false;
+
         waitForStart();
 
         while (opModeIsActive()) {
@@ -54,8 +63,12 @@ public class AUTOTestPracticeBot extends AUTOMecanumAbstractPracticeBot {
                 if (gamepad1.b) speed += 0.00005;
                 if (gamepad1.x) logValues = true;
                 if (gamepad1.y) logValues = false;
+                if (gamepad1.right_bumper) twoSensors = true;
+                if (gamepad1.left_bumper) twoSensors = false;
+
                 telemetry.addData("speed: ", speed);
                 telemetry.addData("Log Values? ", logValues);
+                telemetry.addData("Two Sensors? ", twoSensors);
                 telemetry.addData("Press Start to run test.", "");
                 telemetry.update();
             }
@@ -65,21 +78,34 @@ public class AUTOTestPracticeBot extends AUTOMecanumAbstractPracticeBot {
                 motorBackRight.setPower(speed);
                 motorFrontRight.setPower(speed);
                 motorFrontLeft.setPower(-speed);
+
+                startTime = eTime.milliseconds();
+
                 while (opModeIsActive() && gamepad1.b == false){// && !isColorYellow(sensorColor) && !isColorYellow(sensorColor2) && !isColorYellow(sensorColor3)) {
                     int red1 = sensorColor.red();
                     int green1 = sensorColor.green();
                     int blue1 = sensorColor.blue();
-                    int red2 = sensorColor2.red();
-                    int green2 = sensorColor2.green();
-                    int blue2 = sensorColor2.blue();
-                    int red3 = sensorColor3.red();
-                    int green3 = sensorColor3.green();
-                    int blue3 = sensorColor3.blue();
+                    encoder = motorBackLeft.getCurrentPosition();
+                    reading++;
+                    int red2 = 0;
+                    int green2 = 0;
+                    int blue2 = 0;
 
-                    Log.i("isColorYellow", "Encoder: " + motorBackLeft.getCurrentPosition());
+                    Log.i("isColorYellow", "Reading: " + reading + " Encoder: " + encoder + " Time(ms): " + eTime.milliseconds());
                     Log.i("isColorYellow", "Red1: " + red1 + " Green1: " + green1 + " Blue1: " + blue1);
-                    Log.i("isColorYellow", "Red2: " + red2 + " Green2: " + green2 + " Blue2: " + blue2);
-                    Log.i("isColorYellow", "Red3: " + red3 + " Green3: " + green3 + " Blue3: " + blue3);
+
+                    if (twoSensors) {
+                        red2 = sensorColor2.red();
+                        green2 = sensorColor2.green();
+                        blue2 = sensorColor2.blue();
+
+                        Log.i("isColorYellow", "Red2: " + red2 + " Green2: " + green2 + " Blue2: " + blue2);
+                    }
+                    //int red3 = sensorColor3.red();
+                    //int green3 = sensorColor3.green();
+                    //int blue3 = sensorColor3.blue();
+
+                    //Log.i("isColorYellow", "Red3: " + red3 + " Green3: " + green3 + " Blue3: " + blue3);
 
                     /*
                     if (sensorColor.red() > highestValRed) {
@@ -100,6 +126,9 @@ public class AUTOTestPracticeBot extends AUTOMecanumAbstractPracticeBot {
                     }
                     */
                 }
+
+                endTime = eTime.milliseconds();
+
                 motorBackLeft.setPower(speed/2 * (speed / Math.abs(speed)));
                 motorBackRight.setPower(speed/2 * -(speed / Math.abs(speed)));
                 motorFrontRight.setPower(speed/2 * -(speed / Math.abs(speed)));
@@ -123,6 +152,7 @@ public class AUTOTestPracticeBot extends AUTOMecanumAbstractPracticeBot {
                 stopMotors();
             }
             while (opModeIsActive() && gamepad1.a == false) {
+                /*
                 telemetry.addData("Highest Red: ", highestValRed);
                 telemetry.addData("Green @ Highest Red: ", greenAtHighestRed);
                 telemetry.addData("Blue @ Highest Red: ", blueAtHighestRed);
@@ -134,6 +164,9 @@ public class AUTOTestPracticeBot extends AUTOMecanumAbstractPracticeBot {
                 telemetry.addData("Highest Blue: ", highestValBlue);
                 telemetry.addData("Red @ Highest Blue: ", redAtHighestBlue);
                 telemetry.addData("Green @ Highest Blue: ",greenAtHighestBlue);
+                */
+                telemetry.addData("Encoder units per reading", "" + encoder / reading);
+                telemetry.addData("Milliseconds per reading", (endTime - startTime) / reading);
                 telemetry.update();
             }
         }
